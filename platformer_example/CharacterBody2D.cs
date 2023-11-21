8ae8746dc7bfb8ef90458a0b35d608384f465c4a
Mattias S <matte_1500@hotmail.com>
2023-11-21 23:18:35 +0100

    add movement

--- platformer_example/CharacterBody2D.cs
+++ platformer_example/CharacterBody2D.cs
@@ -3,30 +3,41 @@ using System;
 
 public partial class CharacterBody2D : Godot.CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 30000.0f;
+	public const float JumpVelocity = -600.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		var fDelta = (float)delta;
+		var input = CollectPlayerInput();
+		var physicsContext = new PhysicsContext(
+			Delta: fDelta,
+			Input: input
+		);
 
+		ApplyMovement(physicsContext);
+	}
+
+	private void ApplyMovement(PhysicsContext ctx) {
+		var direction = ctx.Input.Direction;
+		var fDelta = ctx.Delta;
+		var velocity = Velocity;
 		// Add the gravity.
 		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+			velocity.Y += gravity * fDelta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (ctx.Input.Jump && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = direction.X * Speed * fDelta;
 		}
 		else
 		{
@@ -36,4 +47,19 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+
+	private PlayerInput CollectPlayerInput() => new PlayerInput(
+		Direction: Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down"),
+		Jump: Input.IsActionJustPressed("ui_accept")
+	);
 }
+
+public record PhysicsContext(
+	float Delta,
+	PlayerInput Input
+);
+
+public record PlayerInput (
+	Vector2 Direction,
+	bool Jump
+);
