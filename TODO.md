ffdadc295ff24ef545f6c7f3a570758496eaa670
Mattias S <matte_1500@hotmail.com>
2023-11-22 00:43:30 +0100

    add local rollback

--- TODO.md
+++ TODO.md
@@ -1,9 +1,9 @@
 # Todo list
 What the hell am i even trying to accomplish with this repo? I am not so sure to be honest...
 
-- Add simple platforming controls.
-- Record and replay inputs.
-- Add server and handle and spawn players on connection.
-- Sync server and client physics clocks.
-- Send inputs to server
-- Receive state from server and playback.
+- [x] Add simple platforming controls.
+- [ ] Record and replay inputs.
+- [ ] Add server and handle and spawn players on connection.
+- [ ] Sync server and client physics clocks.
+- [ ] Send inputs to server
+- [ ] Receive state from server and playback.
--- platformer_example/CharacterBody2D.cs
+++ platformer_example/CharacterBody2D.cs
@@ -1,37 +1,70 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class CharacterBody2D : Godot.CharacterBody2D
 {
+	public const uint MaxJumps = 1;
 	public const float Speed = 30000.0f;
 	public const float JumpVelocity = -600.0f;
-
+	private uint _jumpsLeft = 1;
+	private List<PhysicsContext> _recordedPhysicsContexts = new List<PhysicsContext>();
+	private Vector2 _positionAtLastRollback = new Vector2();
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
+    public override void _Ready()
+    {
+        base._Ready();
+		_positionAtLastRollback = GlobalPosition;
+    }
+    public override void _PhysicsProcess(double delta)
 	{
 		var fDelta = (float)delta;
-		var input = CollectPlayerInput();
-		var physicsContext = new PhysicsContext(
-			Delta: fDelta,
-			Input: input
-		);
+		var ctx = RecordPhysicsContext(fDelta);
 
-		ApplyMovement(physicsContext);
+		ApplyMovement(ctx);
+
+		if(Input.IsActionJustPressed("rollback"))
+			RollbackMovement();
+	}
+	
+	private void RollbackMovement() {
+		GlobalPosition = _positionAtLastRollback;
+
+		var firstCtx = _recordedPhysicsContexts.First();
+		_jumpsLeft = firstCtx.JumpsLeft;
+		Velocity = firstCtx.Velocity;
+
+		foreach(var ctx in _recordedPhysicsContexts)
+		{
+			ApplyMovement(ctx);
+		}
+		_recordedPhysicsContexts = new List<PhysicsContext>();
+		_positionAtLastRollback = GlobalPosition;
 	}
 
 	private void ApplyMovement(PhysicsContext ctx) {
 		var direction = ctx.Input.Direction;
 		var fDelta = ctx.Delta;
 		var velocity = Velocity;
+
 		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * fDelta;
+		velocity.Y += gravity * fDelta;
 
 		// Handle Jump.
-		if (ctx.Input.Jump && IsOnFloor())
+
+		if(IsOnFloor())
+		{
+			_jumpsLeft = MaxJumps;
+		}
+
+		if (ctx.Input.Jump && ctx.JumpsLeft > 0)
+		{
 			velocity.Y = JumpVelocity;
+			_jumpsLeft -= 1;
+		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
@@ -48,6 +81,19 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
 		MoveAndSlide();
 	}
 
+	private PhysicsContext RecordPhysicsContext(float delta) {
+		var ctx = new PhysicsContext(
+			Delta: delta,
+			Velocity: Velocity,
+			JumpsLeft: _jumpsLeft,
+			Input: CollectPlayerInput()
+		);
+
+		_recordedPhysicsContexts.Add(ctx);
+
+		return ctx;
+	}
+
 	private PlayerInput CollectPlayerInput() => new PlayerInput(
 		Direction: Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down"),
 		Jump: Input.IsActionJustPressed("ui_accept")
@@ -56,6 +102,8 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
 
 public record PhysicsContext(
 	float Delta,
+	Vector2 Velocity,
+	uint JumpsLeft,
 	PlayerInput Input
 );
 
