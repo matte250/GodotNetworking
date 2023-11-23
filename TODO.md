887bd4eee31753e9dbc22458c10e5801d2d3547f
Mattias S <matte_1500@hotmail.com>
2023-11-23 23:28:07 +0100

    Refactor movement

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 What the hell am i even trying to accomplish with this repo? I am not so sure to be honest...
 
 - [x] Add simple platforming controls.
-- [ ] Record and replay inputs.
+- [x] Record and replay inputs.
 - [ ] Add server and handle and spawn players on connection.
 - [ ] Sync server and client physics clocks.
 - [ ] Send inputs to server
--- platformer_example/CharacterBody2D.cs
+++ platformer_example/CharacterBody2D.cs
@@ -5,12 +5,15 @@ using System.Linq;
 
 public partial class CharacterBody2D : Godot.CharacterBody2D
 {
-	public const uint MaxJumps = 1;
+	public const uint MaxJumps = 2;
 	public const float Speed = 30000.0f;
 	public const float JumpVelocity = -600.0f;
-	private uint _jumpsLeft = 1;
 	private List<PhysicsContext> _recordedPhysicsContexts = new List<PhysicsContext>();
-	private Vector2 _positionAtLastRollback = new Vector2();
+	private Vector2 _positionAtLastRollback = Vector2.Zero;
+	private State _prevState = new State(
+		Velocity: Vector2.Zero,
+		JumpsLeft: MaxJumps
+	);
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
@@ -22,76 +25,85 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
     public override void _PhysicsProcess(double delta)
 	{
 		var fDelta = (float)delta;
-		var ctx = RecordPhysicsContext(fDelta);
+		var input = CollectPlayerInput();
+		var state = CalculateState(
+			delta: fDelta,
+			input: input,
+			prevState: _prevState
+		);
 
-		ApplyMovement(ctx);
+		RecordPhysicsContext(fDelta, input, _prevState);
+
+		_prevState = state;
+
+		Velocity = state.Velocity;
+		MoveAndSlide();
 
 		if(Input.IsActionJustPressed("rollback"))
 			RollbackMovement();
+		
 	}
 	
 	private void RollbackMovement() {
 		GlobalPosition = _positionAtLastRollback;
 
-		var firstCtx = _recordedPhysicsContexts.First();
-		_jumpsLeft = firstCtx.JumpsLeft;
-		Velocity = firstCtx.Velocity;
-
 		foreach(var ctx in _recordedPhysicsContexts)
 		{
-			ApplyMovement(ctx);
+			var state = CalculateState(ctx.Delta, ctx.Input, ctx.State);
+			Velocity = state.Velocity;
+			MoveAndSlide();
 		}
+
+		_prevState = _recordedPhysicsContexts.Last().State;
 		_recordedPhysicsContexts = new List<PhysicsContext>();
 		_positionAtLastRollback = GlobalPosition;
 	}
 
-	private void ApplyMovement(PhysicsContext ctx) {
-		var direction = ctx.Input.Direction;
-		var fDelta = ctx.Delta;
-		var velocity = Velocity;
-
-		// Add the gravity.
-		velocity.Y += gravity * fDelta;
-
-		// Handle Jump.
+	private State CalculateState(float delta, PlayerInput input, State prevState) {
+		var direction = input.Direction;
+		var velocity = prevState.Velocity;
+		var jumpsLeft = prevState.JumpsLeft;
 
 		if(IsOnFloor())
 		{
-			_jumpsLeft = MaxJumps;
+			jumpsLeft = MaxJumps;
+			velocity.Y = gravity * delta;
+		} else {
+			// Add the gravity.
+			velocity.Y += gravity * delta;
 		}
 
-		if (ctx.Input.Jump && ctx.JumpsLeft > 0)
+		if (input.Jump && jumpsLeft > 0)
 		{
 			velocity.Y = JumpVelocity;
-			_jumpsLeft -= 1;
+			jumpsLeft -= 1;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed * fDelta;
+			velocity.X = direction.X * Speed * delta;
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
 
-		Velocity = velocity;
-		MoveAndSlide();
+		return new State(
+			Velocity: velocity,
+			JumpsLeft: jumpsLeft
+		);
 	}
 
-	private PhysicsContext RecordPhysicsContext(float delta) {
+	private void RecordPhysicsContext(float delta, PlayerInput input, State state) {
 		var ctx = new PhysicsContext(
 			Delta: delta,
-			Velocity: Velocity,
-			JumpsLeft: _jumpsLeft,
-			Input: CollectPlayerInput()
+			Input: input,
+			State: state
 		);
 
 		_recordedPhysicsContexts.Add(ctx);
-
-		return ctx;
 	}
 
 	private PlayerInput CollectPlayerInput() => new PlayerInput(
@@ -102,9 +114,13 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
 
 public record PhysicsContext(
 	float Delta,
+	PlayerInput Input,
+	State State
+);
+
+public record State(
 	Vector2 Velocity,
-	uint JumpsLeft,
-	PlayerInput Input
+	uint JumpsLeft
 );
 
 public record PlayerInput (
